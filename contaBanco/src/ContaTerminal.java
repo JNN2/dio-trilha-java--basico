238042fc8060eb164501a0ada2179d3070ba7a12
SeuNomeNoGitHub <seuemail@exemplo.com>
2025-07-19 02:39:05 -0300

    meu projeto conta banco

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,25 +1,23 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Por favor, digite o número da Agência:");
-        String agencia = scanner.nextLine();
-
-        System.out.println("Por favor, digite o número da Conta:");
-        int numero = scanner.nextInt();
-        scanner.nextLine(); // Limpa o buffer do teclado após o nextInt()
-
-        System.out.println("Por favor, digite o nome do Cliente:");
-        String nomeCliente = scanner.nextLine();
-
-        System.out.println("Por favor, digite o saldo:");
-        double saldo = scanner.nextDouble();
-
-        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco!%n" + "Sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque.%n", nomeCliente, agencia, numero, saldo);
-
-        scanner.close();
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Por favor, digite o número da Agência:");
+            String agencia = scanner.nextLine();
+            
+            System.out.println("Por favor, digite o número da Conta:");
+            int numero = scanner.nextInt();
+            scanner.nextLine(); // Limpa o buffer do teclado após o nextInt()
+            
+            System.out.println("Por favor, digite o nome do Cliente:");
+            String nomeCliente = scanner.nextLine();
+            
+            System.out.println("Por favor, digite o saldo:");
+            double saldo = scanner.nextDouble();
+            
+            System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco!%n" + "Sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque.%n", nomeCliente, agencia, numero, saldo);
+        }
     }
 }
 
